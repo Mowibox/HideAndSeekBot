c38b212198a285965a094697687647d2f9c99d82
SupMateo <mateo.suprano@ensea.fr>
2023-05-09 17:26:09 +0200

    ajout des fonctions Rotate, TurnAngle, et modif MoveForward
    modification dans se cacher (test)

--- Code/Se cacher (test)/Se cacher/Se cacher/hide.c
+++ Code/Se cacher (test)/Se cacher/Se cacher/hide.c
@@ -6,12 +6,12 @@
 //variables associées aux composants
 int frontCaptor = 0; // distance en cm
 int leftCaptor = 0; // idem
-int rightCaptor; // idem
+int rightCaptor = 0; // idem
 bool leftWheel = 0; // 0 = roule pas, 1 = roule
 bool rightWheel = 0; // idem
 
 //variable de l'algo
-srand(time(NULL)); // Initialisation de la donnée seed pour la valeur aleatoire du nombre de mur rencontré
+srand(time(NULL)); // Initialisation de la donnée seed pour la valeur aleatoire du nombre de mur rencontré (je comprend pas les erreurs, j'ai recopié un exemple de code pour les chiffres aléatoires d'internet)
 const int nbrWallExpected = rand()%5;
 int nbrWall = 0;
 const int dirWall = rand()%2;
@@ -20,14 +20,40 @@ bool isSeekingWall = false;
 bool isSeekingPos = false;
 bool isHidden = false;
 
-void Turn(int deg) {
 
+
+void Rotate(int angle) {
+	if (angle > 0) {
+		//temps de rotation des roues à determiner en fonction de l'angle
+		leftWheel = 1;
+		rightWheel = -1;
+	}
+	if (angle < 0) {
+		//temps de rotation des roues à determiner en fonction de l'angle
+		leftWheel = -1;
+		rightWheel = 1;
+	}
+	 
 }
 
-void TurnAngle(int dir) {
+void TurnAngle(int dir) { //fonction pour faire tourner le robot autour d'un angle d'un mur
+	if (dir == 1) {
+		Rotate(45);
+		MoveForward(1);
+		if (rightCaptor > 10) {
+			Rotate(45);
+		}
+	}
+	else {
+		Rotate(-45);
+		MoveForward(1);
+		if (leftCaptor > 10) {
+			Rotate(-45);
+		}
 
+
+	}
 }
-
 void GetCloser(int dir) {
 
 }
@@ -37,9 +63,12 @@ void RollAway(int dir) {
 }
 
 void MoveForward(float time) {
-	//durant time secondes
+	//durant time secondes :
 	leftWheel = 1;
 	rightWheel = 1;
+	// on arrete ensuite les roues apres time secondes
+	leftWheel = 0;
+	rightWheel = 0;
 }
 
 void SeekAnotherWall() {
@@ -50,6 +79,7 @@ void SeekAnotherWall() {
 			isSeekingWall = false;
 		}
 	}
+
 
 	
 }
@@ -65,8 +95,8 @@ void Hiding() {
 				Turn(rand()%360);
 			}
 		}
-		if (dirWall == 1) { TurnLeft(); }
-		else { TurnRight(); }
+		if (dirWall == 1) { Rotate(-45); }
+		else { TurnRight(45); }
 		isSeekingPos = true;
 		while (isSeekingPos == true) {
 			if (dirWall == 1){
