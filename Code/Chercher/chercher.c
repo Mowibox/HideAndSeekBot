219308a625ee3ec4947759775e6147e95d91c0c3
Emma Verger <emmav.contact@gmail.com>
2023-05-11 11:58:31 +0200

    Amélioration chercher
    Fin du code, ajout de la dimension temporelle

--- Code/Chercher/chercher.c
+++ Code/Chercher/chercher.c
@@ -1,58 +1,120 @@
 #include "fichier.h"
 #include <stdbool.h>
 
-vovoid chercher(){
-	
-	float intensite_max ;
-	float angle_imax ;
-	float valeur_min_IR = ; / à déterminer 
-	float valeur_robot_trouve = ; /à déterminer
-	bool scan_en_cours = false;  /marhce toujours pas?
-	int direction; 
-	
-	while ( capteurIR < valeur_robot_trouve){
-		
-		if (capteurIR >= valeur_min_IR){   
-			arreter_les_roues();
-			intensite_max = capteurIR ; 		
-			angle_imax = angle_servomoteur;	/?		 
-			tourner_angle_correspondant(angle_servomoteur);
-			angle_servomoteur = 0; 				/on réinitialise la position  , prendre code sur internet 
+
+void scan(int g, int d){
+	for (i = g; i <= d; i += ((d-g)/6) ) {
+		if(capteurIR >= intensite_max){
+			intensite_max = capteurIR;
+			angle_imax = angle_servomoteur;
+		}
+	}
+}
 
 
-			// deux prochains blocs à mettre dans une fonction scan_et_tourner()
-			
-			bool scan_en_cours = 1; 
-			while (scan_en_cours == 1 ){
-				scan(); /mettre en paramètre droite ou gauche (de 0 à 90 ou -90 à 0) fonction à deux paramètres
-					if(capteurIR >= intensite_max){ / ça marche tjrs pas en fait, faudrait avoir un while scan???
-						intensité_max = capteurIR;
-						angle_imax = angle_servomoteur;
-					}
-				}
-		
-			if (angle_imax =! 0 ){  / est la valeur initiale : la vmaleur max n'a pas changée
+void scan_et_tourne(int g,int d){  //variables pas encore définies ; paramètres : plage de rotation
+	scan(int g, int d);
+	
+	if (angle_imax =! 0 ){  // est la valeur initiale : la vmaleur max n'a pas changée
 				tourner_angle_correspondant(angle_imax);
 				angle_servomoteur = 0;
-			
-					//elif ?? EN DEHORS DE SCAN ET TOURNE : que fais t il sinon ? avancer tout droit
+	}
+}
+
+void petite_danse(){
+	arreter_les_roues();
+	tour_gauche();
+	tour_droite();
+}
+
+void tour_gauche(){
+	tourner_a_gauche();
+	tourner_a_gauche();
+	tourner_a_gauche();
+	tourner_a_gauche();
+}
+
+
+void tour_droite(){
+	tourner_a_droite();
+	tourner_a_droite();
+	tourner_a_droite();
+	tourner_a_droite();
+}
+
+
+void demi_tour(){
+	tourner_a_gauche();
+	tourner_a_gauche();
+}
+
+
+
+void chercher(){
+	
+	// variables locales : à dénifir en #define dans fichier h ?
+
+	float intensite_max ;
+	float angle_imax ;
+	float valeur_min_IR = ; // à déterminer 
+	float valeur_robot_trouve = ; //à déterminer
+	bool scan_en_cours = false; 
+	bool recherche_infrarouge_enclanchée = false;
+	
+	while ( capteurIR < valeur_robot_trouve){
+
+		avancer_tout_droit(3); //pendant trois secondes (à voir)
+		arreter_les_roues(); // pas necessaire car on n'avance plus mais on sait jamais c'est là
 		
-		/continuer à avancer et à trouver le max jusqu'à trouver capteurIR = valeur robot trouvée
-		// à faire
-	
-	
-		if (capteur_US_front == 1){
+
+		// SCAN IR :
+
+		
+		if (capteurIR >= valeur_min_IR AND recherche_infrarouge_enclanchée = false) {
+			bool recherche_infrarouge_enclanchee = true;	// inutile dans le code actuel mais je garde l'idée au cas où j'en ai besoin 
 			arreter_les_roues();
-			scan_et_tourne(-90, 90); / fait un tour de 180°
-			if (angle_imax ==0){
-				demi_tour(); / pas forcément, voir si à droite ou à gauche on capte un truc)
+			arreter_rotation_servomoteur();
+			intensite_max = capteurIR ; 					 
+			tourner_angle_correspondant(angle_servomoteur);
+			reinitialiser_pos_servomoteur(); 				// prendre code sur internet 
+			angle_imax = 0;
+			scan_et_tourne();
+
+			while ( capteurIR < valeur_robot_trouve){
+				avancer_tout_droit(2);					// problème : et si il ne le trouve pas ? // pdt 2 sec
+			}
+
+			petite_danse();
+			end; // arrêter le programme
+		}
+
+		
+		
+		// Scan US
+
+
+		if (capteur_US_front == 1 AND capteurIR < valeur_robot_trouve){
+
+			if (capteur_US_droit == 1 AND capteur_US_gauche ==0 ){
+				tourner_a_gauche();
+				longer_mur(); // ENCORE UN PBBBBBBBBBBBBBBB j'en peux plus : dans longer mur : il doit scanner
+			}
+
+			if (capteur_US_gauche ==1 AND capteur_US_droit == 0){
+				tourner_a_droite();
+				longer_mur(); // ENCORE UN PBBBBBBBBBBBBBBB j'en peux plus : dans longer mur : il doit scanner
 			}
 		
-		if (capteur_US_right ==1 OR capteur_US_ left == 1){
-			direction(); /retourne la direction droite ou gauche (0 gauche et 1 droite)
-			longer_mur(direction); 
+			if (capteur_US_right ==1 AND capteur_US_ left == 1){
+				demi_tour(); 
+			}
 		}
+		
+
 	petite_danse();
+
+	}
+
 }
 
 	
--- Code/Chercher/fichier.h
+++ Code/Chercher/fichier.h
@@ -1,7 +1,18 @@
-#include "fichier.h"
+#include <stdio.h> 
+#include <stdbool.h>
 
-int main(){
-	cacher();
-	chercher();
-}
-
+void chercher();
+void arreter_les_roues();
+void tourner_angle_correspondant();
+void scan(int g,int d);
+void scan_et_tourne(int g,int d);
+void demi_tour();
+void petite_danse();
+void arreter_rotation_servomoteur();
+void reinitialiser_pos_servomoteur();
+void longer_mur(); 
+void tour_gauche();
+void tourner_a_gauche();
+void tourner_a_droite();
+void tour_droite();
+void avancer_tout_droit(int duree);
